ff03c7a56e49836cf82278e85934ac88699c2e5d
curtisosborn <46169753+curtisosborn@users.noreply.github.com>
2019-06-07 15:52:15 -0500

    Changed button logic

--- app.js
+++ app.js
@@ -10,29 +10,28 @@ for (let i = 0; i < whenBtns.length; i++) {
   whenBtns[i].addEventListener("click", function() {
     console.log(whenBtns[i].innerHTML);
     switch (whenBtns[i].innerHTML) {
-      case "-":
-        whenBtns[i].classList.add("before");
-        whenBtns[i].innerHTML = "Before";
-        break;
-      case "Before":
-        whenBtns[i].classList.add("tied");
-        whenBtns[i].classList.remove("before");
-        whenBtns[i].innerHTML = "Tied";
-        break;
-      case "Tied":
-        whenBtns[i].classList.add("adjusted");
-        whenBtns[i].classList.remove("tied");
-        whenBtns[i].innerHTML = "Adjusted";
-        break;
-      case "Adjusted":
+      case "After/Adjusted":
         whenBtns[i].classList.add("after");
-        whenBtns[i].classList.remove("adjusted");
         whenBtns[i].innerHTML = "After";
         break;
       case "After":
+        whenBtns[i].classList.add("adjusted");
+        whenBtns[i].classList.remove("after");
+        whenBtns[i].innerHTML = "Adjusted";
+        break;
+      case "Adjusted":
+        whenBtns[i].classList.remove("adjusted");
+        whenBtns[i].innerHTML = "After/Adjusted";
+        break;
+      case "-":
+        whenBtns[i].classList.add("after");
+        whenBtns[i].innerHTML = " After ";
+        break;
+      case " After ":
         whenBtns[i].classList.remove("after");
         whenBtns[i].innerHTML = "-";
         break;
+
       default:
         break;
     }
@@ -50,6 +49,18 @@ for (let i = 0; i < arrowBtns.length; i++) {
         break;
       case "fas fa-arrow-right fa-lg dark-grey":
         arrowBtns[i].classList.value = "fas fa-arrow-right fa-lg green";
+        break;
+      case "fas fa-arrow-right fa-lg dark-grey mw":
+        arrowBtns[i].classList.value = "fas fa-arrow-right fa-lg mw red";
+        break;
+      case "fas fa-arrow-right fa-lg mw red":
+        arrowBtns[i].classList.value = "fas fa-arrow-right fa-lg dark-grey mw";
+        break;
+      case "fas fa-arrow-left fa-lg dark-grey mw":
+        arrowBtns[i].classList.value = "fas fa-arrow-left fa-lg mw red";
+        break;
+      case "fas fa-arrow-left fa-lg mw red":
+        arrowBtns[i].classList.value = "fas fa-arrow-left fa-lg dark-grey mw";
         break;
       case "fas fa-arrow-right fa-lg green":
         arrowBtns[i].classList.value = "fas fa-arrow-right fa-lg dark-grey";
